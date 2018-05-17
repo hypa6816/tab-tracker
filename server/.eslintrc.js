62354ea5ecaa392791f31f2fadf4b9a7d66af8f3
Sara Park <sara.park@polycom.com>
2018-05-17 17:00:51 -0600

    express js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "standard",
+    rules: {
+        // allow async-await
+        'generator-star-spacing': 'off',
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'indent': 0
+      }
+};
